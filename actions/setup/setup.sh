b18f207864f65d537f02f930250893f447060940
chensoul <ichensoul@gmail.com>
2025-05-23 15:48:28 +0800

    chore: 更新构建工作流和设置脚本
    - 修改 .github/workflows/build.yml，调整 codecov token 的使用方式
    - 新增 actions/setup/setup.sh，添加项目构建所需的环境变量设置
    - 新增 actions/setup/action.yml，定义 setup action 的输出参数

---
+++ actions/setup/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "::group::Setup preparing"
+
+SONAR=$(echo "MDhiYmNiZDM4ZTJkZmQ5ODdmY2NlNzEzMDVkOGNhZTZmYTA5YTkyOAo=" | openssl base64 -d)
+echo "sonar=${SONAR}" >> $GITHUB_OUTPUT
+
+DATE_YM=$(date +%y%m)
+echo "date-ym=${DATE_YM}" >> $GITHUB_OUTPUT
+echo "date-ym=${DATE_YM}"
+
+if [ -z "${GITHUB_REF##*heads*}" ]; then
+  BRANCH=${GITHUB_REF#refs/heads/}
+  echo "branch=${BRANCH}" >> $GITHUB_OUTPUT
+  echo "branch=${BRANCH}"
+fi
+
+if [ -z "${GITHUB_REF##*tags*}" ]; then
+  TAG=${GITHUB_REF#refs/tags/}
+  echo "tag=${TAG}" >> $GITHUB_OUTPUT
+  echo "tag=${TAG}"
+fi
+
+echo "::endgroup::"

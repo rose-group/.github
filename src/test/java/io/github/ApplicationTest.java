637d4011a544b64e3e21571cc0a5bd7673acc607
chensoul <ichensoul@gmail.com>
2025-06-11 08:52:21 +0800

    ci: 更新 Maven 构建和发布工作流
    - 移除了 maven-build.yml 中的 Sonar 相关配置
    - 修改了 maven-release.yml 的名称和发布条件

---
+++ src/test/java/io/github/ApplicationTest.java
@@ -0,0 +1,18 @@
+package io.github;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * TODO Comment
+ *
+ * @author <a href="mailto:ichensoul@gmail.com">chensoul</a>
+ * @since TODO
+ */
+class ApplicationTest {
+    @org.junit.jupiter.api.Test
+    void main() {
+        Assertions.assertTrue(1 == 1);
+    }
+}

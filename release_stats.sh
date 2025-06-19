a4fc7739f85c34936b2be14d4c23fdaf1907e486
chensoul <ichensoul@gmail.com>
2025-06-19 12:04:54 +0800

    Merge remote-tracking branch 'origin/main'

---
+++ release_stats.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+curl   -H "Accept: application/vnd.github.v3+json"   https://api.github.com/repos/rosestack/.github/releases| jq -r '.[] | (.tag_name + "," + (.assets[]|(.name+","+(.download_count|tostring))))' | grep -v \.asc | sort
